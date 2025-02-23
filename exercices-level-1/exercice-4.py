3e3570bcc4ad5a409d8e1bfa279c19a2f621bfa4
rlr6s <ronald3025@outlook.com>
2025-02-23 16:22:40 -0400

    Update exercice-4.py

--- exercices-level-1/exercice-4.py
+++ exercices-level-1/exercice-4.py
@@ -1,2 +1,21 @@
 '''Escribir una función que tome un carácter y devuelva True si es una vocal, de lo contrario
 devuelve False.'''
+
+def main():
+  si_o_no = vocal()
+  print(si_o_no)
+  
+def vocal():
+  while True:
+    try:
+        caracter = input("introduce un caracter: ")
+        if caracter.lower() in "aeiou":
+            return True
+        else:
+            print("no es una vocal")
+    except EOFError:
+        pass       
+        break
+    
+main()
+
