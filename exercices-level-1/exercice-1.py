fc76dc596184f917d8d5325e6a6e2f6a865c9a9e
rlr6s <ronald3025@outlook.com>
2025-02-23 14:03:58 -0400

    Update exercice-1.py

--- exercices-level-1/exercice-1.py
+++ exercices-level-1/exercice-1.py
@@ -1,3 +1,18 @@
 '''Definir una función max() que tome como argumento dos números y devuelva el mayor de ellos.
 Es cierto que python tiene una función max() incorporada, pero hacerla nosotros mismos es un
 muy buen ejercicio'''
+
+
+def main():
+    print(max())
+
+def max():
+    a = int(input("Ingrese el primer número: "))
+    b = int(input("Ingrese el segundo número: "))
+    if a > b:
+        return a
+    else:
+        return b
+    
+
+main()
