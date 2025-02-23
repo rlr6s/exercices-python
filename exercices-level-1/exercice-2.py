8ac1b8a396c2c966f47ad1de0f88129c95b4ce0d
rlr6s <ronald3025@outlook.com>
2025-02-23 15:52:32 -0400

    Update exercice-2.py

--- exercices-level-1/exercice-2.py
+++ exercices-level-1/exercice-2.py
@@ -1,2 +1,29 @@
 '''Definir una función max_de_tres(), que tome tres números como argumentos y devuelva el
 mayor de ellos. '''
+
+
+def main():
+  mayor = max_de_tres()
+
+  print(f"El numero mas grande es {mayor}")
+
+def max_de_tres(mayor):
+  a = int(input("ingrese el primer numero: "))
+  b = int(input("ingrese el segundo numero: "))
+  c = int(input("ingrese el tercer numero: "))
+
+  if a > b and a > c:
+        
+        mayor = a
+  elif b > c:
+        mayor = b
+        
+  else: 
+       mayor = c
+
+  return mayor
+    
+
+
+main()
+
