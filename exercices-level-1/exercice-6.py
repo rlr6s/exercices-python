382050ff51a086e89647f8c2757a1d49b1823d79
rlr6s <ronald3025@outlook.com>
2025-02-27 22:46:07 -0400

    Update exercice-6.py

--- exercices-level-1/exercice-6.py
+++ exercices-level-1/exercice-6.py
@@ -1,2 +1,18 @@
 '''Definir una función inversa() que calcule la inversión de una cadena. Por ejemplo la cadena
 "estoy probando" debería devolver la cadena "odnaborp yotse"'''
+
+
+def main():
+    cadena = input("Ingrese una cadena: ")
+    cadena_inversa = inversa(cadena)
+    print(cadena_inversa)
+
+def inversa(cadena):
+    
+    return cadena[::-1] 
+
+     
+
+  
+main()
+
