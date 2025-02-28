272a67eb4c70940affd6d1062803cd9cd7d90107
rlr6s <ronald3025@outlook.com>
2025-02-27 23:10:49 -0400

    Update exercice-7.py

--- exercices-level-1/exercice-7.py
+++ exercices-level-1/exercice-7.py
@@ -1,2 +1,16 @@
 '''Definir una función es_palindromo() que reconoce palíndromos (es decir, palabras que tienen el
 mismo aspecto escritas invertidas), ejemplo: es_palindromo ("radar") tendría que devolver True.'''
+
+
+def main():
+    palabra = input("ingrese una palabra: ")
+    print(palindromo(palabra))
+
+def palindromo(palabra):
+    if palabra == palabra[::-1]:
+        return True
+    else:
+        return False
+    
+
+main()
