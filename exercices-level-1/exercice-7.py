f639e131859e3c356ad46e215fcceaf4ce5cb21a
rlr6s <ronald3025@outlook.com>
2025-02-27 23:01:02 -0400

    Update exercice-7.py

--- exercices-level-1/exercice-7.py
+++ exercices-level-1/exercice-7.py
@@ -1,2 +1,20 @@
 '''Definir una función es_palindromo() que reconoce palíndromos (es decir, palabras que tienen el
 mismo aspecto escritas invertidas), ejemplo: es_palindromo ("radar") tendría que devolver True.'''
+
+
+def main():
+    cadena = input("Ingrese una cadena: ")
+    cadena_inversa = inversa(cadena)
+    if es_palindromo(cadena):
+        print("Es palindromo")
+    else:
+        print("No es palindromo")
+def inversa(cadena):
+    
+    return cadena[::-1] 
+
+def es_palindromo(cadena):
+    return cadena == inversa(cadena)   
+
+  
+main()
