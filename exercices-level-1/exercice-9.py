84cfbfe12ece80952720678739570f03d4c31075
rlr6s <ronald3025@outlook.com>
2025-02-27 23:19:35 -0400

    Update exercice-9.py

--- exercices-level-1/exercice-9.py
+++ exercices-level-1/exercice-9.py
@@ -1,2 +1,14 @@
 '''Definir una función generar_n_caracteres() que tome un entero n y devuelva el caracter
 multiplicado por n. Por ejemplo: generar_n_caracteres(5, "x") debería devolver "xxxxx".'''
+
+
+
+def main():
+   caracter = (input("ingrese el caracter: "))
+   n = int(input("ingrese el numero de veces que se repetira el caracter: "))
+   print(generar_n_caracteres(n, caracter))
+
+def generar_n_caracteres(n, caracter):
+    return caracter * n
+
+main()
