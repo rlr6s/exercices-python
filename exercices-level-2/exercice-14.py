cb8cf50950d9a86e282ad1a11be2a06df98c912e
rlr6s <ronald3025@outlook.com>
2025-03-17 20:47:38 -0400

    Update exercice-14.py

--- exercices-level-2/exercice-14.py
+++ exercices-level-2/exercice-14.py
@@ -1,2 +1,8 @@
 '''Escribir un programa que pregunte al usuario por el número de horas trabajadas y el coste por hora. 
 Después debe mostrar por pantalla la paga que le corresponde.'''
+
+horas = int(input("cuantas horas trabaja al di?: "
+pagoxh = int(input("cuanto le pagan por hora?" 
+
+pagatotal = horas * pagoxh 
+print(f'a usted le pagan al final del dia:{pagatotal}")
