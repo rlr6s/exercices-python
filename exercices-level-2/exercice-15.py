51232f965a25dbd4067c279f02c41dc530c7a4c4
rlr6s <ronald3025@outlook.com>
2025-03-17 21:04:58 -0400

    Update exercice-15.py

--- exercices-level-2/exercice-15.py
+++ exercices-level-2/exercice-15.py
@@ -3,3 +3,15 @@
   y muestre por pantalla la frase Tu índice de masa corporal es <imc> donde <imc> es el
    índice de masa corporal calculado
  redondeado con dos decimales. '''
+
+
+def main():
+    peso = float(input("cuanto pesas en kg:"))
+    altura = float(input("cuanto mides en cm:"))
+    
+    altura2 = altura * altura
+    imc = peso / altura2
+
+    print(f"Tu índice de masa corporal es {imc:.2f} ")
+
+main()
