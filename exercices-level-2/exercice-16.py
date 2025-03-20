e1cda903529cbf30acd32118eff834c53a18c3a3
rlr6s <ronald3025@outlook.com>
2025-03-20 09:21:10 -0400

    Update exercice-16.py

--- exercices-level-2/exercice-16.py
+++ exercices-level-2/exercice-16.py
@@ -2,3 +2,13 @@
 muestre por pantalla la <n> entre <m> da un cociente <c> y un resto <r> donde <n> y <m> son 
 los números introducidos por el usuario, y <c> y <r> son el cociente y el resto de la división
  entera respectivamente. '''
+
+m = float(input("ingrese un numero: "))
+n = float(input("ingrese un numero: "))
+
+c = n // m
+r = n % m
+
+print(f"cociente es {c}")
+print(f'el resto es {r}')
+          
