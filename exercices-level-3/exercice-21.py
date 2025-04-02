773dacbdad8f289f4d842b5ba7c7a20c8e5d7801
rlr6s <ronald3025@outlook.com>
2025-04-02 11:50:58 -0400

    Update exercice-21.py

--- exercices-level-3/exercice-21.py
+++ exercices-level-3/exercice-21.py
@@ -1,3 +1,11 @@
 '''Escribir un programa que pregunte el nombre del usuario en la consola y un número entero e 
 imprima por pantalla en líneas distintas el nombre del usuario tantas veces como 
 el número introducido.'''
+
+def main():
+    nombres = input("Ingrese su nombre: ")
+    n = int(input("Ingrese un número entero: "))
+
+    for _ in range(n):
+        print(nombres)
+main()
